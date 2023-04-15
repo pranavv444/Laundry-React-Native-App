12c2552ebaff9eac5b7aaf682cde1139366739e0
pranavv444 <95160361+pranavv444@users.noreply.github.com>
2023-04-15 22:11:57 +0530

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -2,92 +2,56 @@ import {
   StyleSheet,
   Text,
   View,
-  SafeAreaView,
-  KeyboardAvoidingView,
   Pressable,
   TextInput,
-  Alert,
+  SafeAreaView,
+  KeyboardAvoidingView,
+  Alert
 } from "react-native";
-import React, { useState } from "react";
-import Constants from "expo-constants";
-import { MaterialIcons } from "@expo/vector-icons";
+import { Feather } from '@expo/vector-icons';
 import { Ionicons } from "@expo/vector-icons";
-import { Feather } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useNavigation } from "@react-navigation/native";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
+const RegisterScreen = () => {
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [phone,setPhone] = useState("");
+    const navigation = useNavigation();
+    const register = () => {
+      if(email === "" || password === "" || phone === ""){
+        Alert.alert(
+          "Invalid Details",
+          "Please fill all the details",
+          [
+            {
+              text: "Cancel",
+              onPress: () => console.log("Cancel Pressed"),
+              style: "cancel"
+            },
+            { text: "OK", onPress: () => console.log("OK Pressed") }
+          ],
+          { cancelable: false }
+        );
+      }
+      createUserWithEmailAndPassword(auth,email,password).then((userCredential) => {
+        console.log("user credential",userCredential);
+        const user = userCredential._tokenResponse.email;
+        const myUserUid = auth.currentUser.uid;
 
-//firebase
-// import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth ,db} from "../firebase";
-// import { setDoc } from "firebase/firestore";
-
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-
-
-
-import { collection, addDoc } from "firebase/firestore"; 
-
-
-
-const  RegisterScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [phone, setPhone] = useState("");
-  const navigation = useNavigation();
-  
-  
-  
-   const  register = async () => {
-    if (email === "" || password === "" || phone === "") {
-      Alert.alert("Invalid Details", "Please fill all the details", [
-        {
-          text: "Cancel",
-          onPress: () => console.log("Cancel Pressed"),
-          style: "cancel",
-        },
-        { text: "OK", onPress: () => console.log("OK Pressed") },
-      ]);
+        setDoc(doc(db,"users",`${myUserUid}`),{
+          email:user,
+          phone:phone
+        })
+      })
     }
-
-
-
-
-
-
-    //firestore 
-const data={
-  email: email,
-  password: password,
-  phone: phone
-}
-    try {
-      const docRef = await addDoc(collection(db, "users"), data);
-      console.log("Document written with ID: ", docRef.id);
-          console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
-
-
-    //auth in firebase
-    // createUserWithEmailAndPassword(auth, email, password).then(
-    //   (userCredential) => {
-    //     console.log("user credentail", userCredential);
-    //     const user = userCredential._tokenRespnse.email;
-    //     const myUserUId = auth.currentUser.uid;
-
-    //     setDoc(doc(db, "users", `${myUserUId}`), {
-    //       email: user,
-    //       phone: phone,
-    //     });
-    //   }
-    // );
-  };
   return (
     <SafeAreaView
       style={{
         flex: 1,
-        marginTop: Constants.statusBarHeight,
         backgroundColor: "white",
         alignItems: "center",
         padding: 10,
@@ -104,6 +68,7 @@ const data={
           <Text style={{ fontSize: 20, color: "#662d91", fontWeight: "bold" }}>
             Register
           </Text>
+
           <Text style={{ fontSize: 18, marginTop: 8, fontWeight: "600" }}>
             Create a new Account
           </Text>
@@ -111,62 +76,66 @@ const data={
 
         <View style={{ marginTop: 50 }}>
           <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <MaterialIcons name="email" size={24} color="black" />
+            <MaterialCommunityIcons
+              name="email-outline"
+              size={24}
+              color="black"
+            />
             <TextInput
               placeholder="Email"
               value={email}
-              onChange={(text) => setEmail(text)}
+              onChangeText={(text) => setEmail(text)}
               placeholderTextColor="black"
               style={{
-                fontSize: password ? 18 : 18,
+                fontSize: email ? 18 : 18,
                 borderBottomWidth: 1,
                 borderBottomColor: "gray",
+                marginLeft: 13,
                 width: 300,
                 marginVertical: 10,
-                marginLeft: 15,
               }}
-            ></TextInput>
+            />
           </View>
 
           <View style={{ flexDirection: "row", alignItems: "center" }}>
             <Ionicons name="key-outline" size={24} color="black" />
             <TextInput
-              placeholder="Password"
               value={password}
-              onChange={(text) => setPassword(text)}
+              onChangeText={(text) => setPassword(text)}
               secureTextEntry={true}
+              placeholder="Password"
               placeholderTextColor="black"
               style={{
                 fontSize: password ? 18 : 18,
                 borderBottomWidth: 1,
                 borderBottomColor: "gray",
+                marginLeft: 13,
                 width: 300,
-                marginVertical: 25,
-                marginLeft: 15,
+                marginVertical: 20,
               }}
-            ></TextInput>
+            />
           </View>
 
           <View style={{ flexDirection: "row", alignItems: "center" }}>
-            <Feather name="phone" size={24} color="black" />
+          <Feather name="phone" size={24} color="black" />
             <TextInput
-              placeholder="Phone"
               value={phone}
-              onChange={(text) => setPhone(text)}
+              onChangeText={(text) => setPhone(text)}
+              placeholder="Phone No"
               placeholderTextColor="black"
               style={{
                 fontSize: password ? 18 : 18,
                 borderBottomWidth: 1,
                 borderBottomColor: "gray",
+                marginLeft: 13,
                 width: 300,
-                marginVertical: 15,
-                marginLeft: 15,
+                marginVertical: 10,
               }}
-            ></TextInput>
+            />
           </View>
 
           <Pressable
-            onPress={register}
+          onPress={register}
             style={{
               width: 200,
               backgroundColor: "#318CE7",
@@ -182,16 +151,16 @@ const data={
             </Text>
           </Pressable>
 
-          <Pressable onPress={() => navigation.goBack()} style={{ margin: 20 }}>
+          <Pressable onPress={() => navigation.goBack()} style={{ marginTop: 20 }}>
             <Text
               style={{
-                fontSize: 16,
                 textAlign: "center",
-                color: "black",
+                fontSize: 17,
+                color: "gray",
                 fontWeight: "500",
               }}
             >
-              Already have an account? Sign In
+              Already have a account? Sign in
             </Text>
           </Pressable>
         </View>
